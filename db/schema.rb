97641454721fce13589f365555238bfde954b383
Jeff Bridges <jbridges1119@gmail.com>
2022-08-01 21:31:02 +0000

    Add product tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_184338) do
+ActiveRecord::Schema.define(version: 2022_08_01_205706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,8 @@ ActiveRecord::Schema.define(version: 2022_07_28_184338) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first"
+    t.string "last"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+   it 'validates all presence ' do
+    @category = Category.new
+    @product = Product.new(name:"name", price: 3.99, quantity: 5, category: @category)
+    @product.save!
+    expect(@product.id).to be_present
+   end
+   it 'validates name presence ' do
+   @category = Category.new
+      @product = Product.new(name: nil, price: 3.99, quantity: 5, category: @category)
+      @product.validate
+      expect(@product.errors.full_messages).to include("Name can't be blank") 
+    end
+   it 'validates price presence ' do
+    @category = Category.new
+    @product = Product.new(name: "name", price: [nil], quantity: 5, category: @category)
+    @product.validate
+    expect(@product.errors.full_messages).to include("Price can't be blank") 
+  end
+   
+   it 'validates quantity presence ' do
+    @category = Category.new
+    @product = Product.new(name:"name", price: 3.99, quantity: nil, category: @category)
+    @product.validate
+    expect(@product.errors.full_messages).to include("Quantity can't be blank") 
+   end
+   it 'validates category presence ' do
+    @category = Category.new
+    @product = Product.new(name:"name", price: 3.99, quantity: 5, category: nil)
+    @product.validate
+    expect(@product.errors.full_messages).to include("Category can't be blank") 
+   end
+  end
+end
+
+
+# describe '#id' do
+#   it 'should not exist for new records' do
+#     @widget = Widget.new
+#     expect(@widget.id).to be_nil
+#   end
+
+#   it 'should be auto-assigned by AR for saved records' do
+#     @widget = Widget.new
+#     # we use bang here b/c we want our spec to fail if save fails (due to validations)
+#     # we are not testing for successful save so we have to assume it will be successful
+#     @widget.save!
+
+#     expect(@widget.id).to be_present
+#   end
+# end
