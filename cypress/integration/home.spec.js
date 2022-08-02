beedb9e74207b7f2744fd771403ec817beeca00a
Jeff Bridges <jbridges1119@gmail.com>
2022-08-02 15:50:29 +0000

    Add Cypress homepage tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe("testing", () => {
+  it("Navigates to homepage", () => {
+    cy.visit("/");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+  
+});
